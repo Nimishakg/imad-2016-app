046b72f9a586195a4bd99aafea01b4d330366085
Nimishakg <nimisha@quizent.com>
2016-09-26 16:39:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var content = {
-    title : 'Article One - Nimisha',
-    heading : 'Article one',
+var articleTwo = {
+    title : 'Article Two - Nimisha',
+    heading : 'Article Two',
     date : 'Sep 5, 2016',
     content : `<p>
                 This is my fsdsd Article. This is my first Article. This is my first Article. This is my first Article. This is my first Article.  
@@ -19,6 +19,40 @@ var content = {
                 This is my first Article. This is my first Article. This is my first Article. This is my first Article. This is my first Article.  
             </p>`
 };
+function createTemplate(data)
+{
+var title = data.title;    
+var heading = data.heading;
+var date = data.date;
+var content = data.content;
+var htmlTemplate = `
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+      <div class="container">
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+        <h3>${heading}</h3>
+        <div>
+            ${date}
+        </div>
+        <div>
+           ${content}
+        </div>
+      </div>
+    </body>
+</html>
+`;
+return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -30,7 +64,7 @@ app.get('/ui/article-one.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+  res.send(createTemplate(articleTwo));
 });
 app.get('/article-three', function (req, res) {
   res.send('Article three requested and will be served here');
