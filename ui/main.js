071caac133af779536faaa0ca45aadba87d2ccea
Nimishakg <nimisha@quizent.com>
2016-09-29 13:26:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element = document.getElementById("text-new");
 element.innerHTML = "New text";
+var image = document.getElementById("madi");
+image.onclick = function()
+{
+    image.style.marginLeft = "100px";
+};
