db8fb7e1e6e4ae4ac7aa54d831ea364aeeaed78a
Nimishakg <nimisha@quizent.com>
2016-09-29 15:11:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,13 @@ console.log('Loaded!');
 var element = document.getElementById("text-new");
 element.innerHTML = "New text";
 var image = document.getElementById("madi");
+var marginLeft = 0;
+function moveRight()
+{
+ marginLeft = marginLeft + 10;
+ image.style.marginLeft = marginLeft + "px";
+}
 image.onclick = function()
 {
-    image.style.marginLeft = "100px";
+   var interval = setInterval(moveRight , 100);
 };
