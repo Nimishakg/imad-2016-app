476d7f7a8b5f1ed009ea57cb1c28878a37488765
Nimishakg <nimisha@quizent.com>
2016-10-06 14:14:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ var button = document.getElementById("counter");
 var counter = 0;
 button.onclick = function()
 {
-  var request = XMLhttpRequest();
+  //var request = XMLhttpRequest();
   
   
  counter = counter + 1;
