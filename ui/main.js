2d77e9fb6bf97e1d8efc008649bd76beb1d130cd
Nimishakg <nimisha@quizent.com>
2016-11-18 04:24:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,33 @@
+//counter code
 var button = document.getElementById("counter");
-var counter = 0;
 button.onclick = function()
 {
-
+ //create a request object    
+  var request = new XMLHttpRequest();
+  //capture response and store it in a variable
+  request.onreadystatechange = function()
+   {
+    //take some action
+    if(request.readyState === XMLHttpRequest.DOM)  
+  
+    {
+        
+     if(request.status===200)
+     
+     {
            
-           counter = counter + 1;
-            var span = document.getElementById("count");
-            span.innerHTML = counter.toString();
+      var counter = request.responseText;
+    
+      var span = document.getElementById("count");
+        
+      span.innerHTML = counter.toString();
+     
+   }
+    }
+  };
+
+request.open('GET','http://nimishakg.imad.hasura-app.io/counter',true);
+
+request.send(null);
 
 };
